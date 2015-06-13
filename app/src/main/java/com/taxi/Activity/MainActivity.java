6eb6ae98caf75dc7b986bd819fe5d12d07a5354b
gvsharma <gvsharma4@gmail.com>
2015-06-13 18:44:39 +0530

    Added some packages

--- app/src/main/java/com/taxi/Activity/MainActivity.java
+++ app/src/main/java/com/taxi/Activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.taxi;
+package com.taxi.Activity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.taxi.R;
 
 
 public class MainActivity extends ActionBarActivity {
