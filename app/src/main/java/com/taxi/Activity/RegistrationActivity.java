ce29ce391320b9cd3921342b48dcc4bff8668cb8
gvsharma <gvsharma4@gmail.com>
2015-06-19 23:10:08 +0530

    login

--- app/src/main/java/com/taxi/Activity/RegistrationActivity.java
+++ app/src/main/java/com/taxi/Activity/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.taxi.Activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,7 +14,7 @@ import android.widget.Toast;
 import com.taxi.R;
 import com.taxi.utils.FloatingHintEditText;
 
-public class RegistrationActivity extends AbstractTaxiActivity {
+public class RegistrationActivity extends AbstractTaxiActivity implements View.OnFocusChangeListener, View.OnClickListener {
 
     private FloatingHintEditText edEmail;
     private FloatingHintEditText edSetPwd;
@@ -28,104 +29,13 @@ public class RegistrationActivity extends AbstractTaxiActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         init();
-        edEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(edEmail.getText().toString().trim().matches("")){
-                        tvValidation.setVisibility(View.VISIBLE);
-                        tvValidation.setText("*invalid email address");
-                    }else{
-                        tvValidation.setVisibility(View.GONE);
-                    }
-
-                }
-
-            }
-        });
-        edSetPwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(edSetPwd.getText().toString().trim().matches("")){
-                        tvValidation.setVisibility(View.VISIBLE);
-                        tvValidation.setText("*Password fields shouldn't be blank");
-                    }else{
-                        tvValidation.setVisibility(View.GONE);
-                    }
-
-                }
-
-            }
-        });
-        edRenterPwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(edRenterPwd.getText().toString().trim().matches("")){
-                        tvValidation.setVisibility(View.VISIBLE);
-                        tvValidation.setText("*Password fields shouldn't be blank");
-                    }else{
-                        tvValidation.setVisibility(View.GONE);
-                    }
-
-                }
-
-            }
-        });
-        edFullName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(edFullName.getText().toString().trim().matches("")){
-                        tvValidation.setVisibility(View.VISIBLE);
-                        tvValidation.setText("*Invalid name");
-                    }else{
-                        tvValidation.setVisibility(View.GONE);
-                    }
-
-                }
-
-            }
-        });
-        edMobNum.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(edMobNum.getText().toString().trim().matches("")){
-                        tvValidation.setVisibility(View.VISIBLE);
-                        tvValidation.setText("*Invalid mobile number");
-                    }else{
-                        tvValidation.setVisibility(View.GONE);
-                    }
-
-                }
-
-            }
-        });
-        edRefCode.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(edRefCode.getText().toString().trim().matches("")){
-                        tvValidation.setVisibility(View.VISIBLE);
-                        tvValidation.setText("*Invalid referral code");
-                    }else{
-                        tvValidation.setVisibility(View.GONE);
-                    }
-
-                }
-
-            }
-        });
-        imgReferralCode.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(RegistrationActivity.this, "Yet to implement", Toast.LENGTH_LONG).show();
-            }
-        });
+        edEmail.setOnFocusChangeListener(this);
+        edSetPwd.setOnFocusChangeListener(this);
+        edRenterPwd.setOnFocusChangeListener(this);
+        edFullName.setOnFocusChangeListener(this);
+        edMobNum.setOnFocusChangeListener(this);
+        edRefCode.setOnFocusChangeListener(this);
+        imgReferralCode.setOnClickListener(this);
     }
 
     private void init(){
@@ -137,6 +47,93 @@ public class RegistrationActivity extends AbstractTaxiActivity {
         edRefCode = (FloatingHintEditText)findViewById(R.id.referrel_code_et);
         tvValidation = (TextView)findViewById(R.id.validation_tv);
         imgReferralCode = (ImageView)findViewById(R.id.referral_iv);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        switch (v.getId()) {
+            case R.id.email_et:
+                if(!hasFocus){
+                    if(edEmail.getText().toString().trim().matches("")){
+                        tvValidation.setVisibility(View.VISIBLE);
+                        tvValidation.setText("*invalid email address");
+                    }else{
+                        tvValidation.setVisibility(View.GONE);
+                    }
+
+                }
+                break;
+            case R.id.set_pwd_et:
+                if(!hasFocus){
+                    if(edSetPwd.getText().toString().trim().matches("")){
+                        tvValidation.setVisibility(View.VISIBLE);
+                        tvValidation.setText("*Password fields shouldn't be blank");
+                    }else{
+                        tvValidation.setVisibility(View.GONE);
+                    }
+
+                }
+                break;
+            case R.id.re_enter_pwd_et:
+                if(!hasFocus){
+                    if(edRenterPwd.getText().toString().trim().matches("")){
+                        tvValidation.setVisibility(View.VISIBLE);
+                        tvValidation.setText("*Password fields shouldn't be blank");
+                    }else{
+                        tvValidation.setVisibility(View.GONE);
+                    }
+
+                }
+                break;
+            case R.id.full_name_et:
+                if(!hasFocus){
+                    if(edFullName.getText().toString().trim().matches("")){
+                        tvValidation.setVisibility(View.VISIBLE);
+                        tvValidation.setText("*Invalid name");
+                    }else{
+                        tvValidation.setVisibility(View.GONE);
+                    }
+
+                }
+                break;
+            case R.id.enter_mob_no_et:
+                if(!hasFocus){
+                    if(edMobNum.getText().toString().trim().matches("")){
+                        tvValidation.setVisibility(View.VISIBLE);
+                        tvValidation.setText("*Invalid mobile number");
+                    }else{
+                        tvValidation.setVisibility(View.GONE);
+                    }
+
+                }
+                break;
+            case R.id.referrel_code_et:
+                if(!hasFocus){
+                    if(edRefCode.getText().toString().trim().matches("")){
+                        tvValidation.setVisibility(View.VISIBLE);
+                        tvValidation.setText("*Invalid referral code");
+                    }else{
+                        tvValidation.setVisibility(View.GONE);
+                    }
+                    if(!hasFocus){
+                        if(edFullName.getText().toString().trim().matches("")){
+                            tvValidation.setVisibility(View.VISIBLE);
+                            tvValidation.setText("*Invalid name");
+                        }else{
+                            tvValidation.setVisibility(View.GONE);
+                        }
+
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.validation_tv) {
+            showMessage("Yet to implement..!");
+        }
     }
 }
 
