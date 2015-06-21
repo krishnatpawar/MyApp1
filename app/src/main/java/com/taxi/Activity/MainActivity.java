caa5347ce512c442edb9bdb02d1d2a6b92d6241e
gvsharma <gvsharma4@gmail.com>
2015-06-21 22:12:15 +0530

    changes main screen..activity_start

--- app/src/main/java/com/taxi/Activity/MainActivity.java
+++ app/src/main/java/com/taxi/Activity/MainActivity.java
@@ -4,11 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
